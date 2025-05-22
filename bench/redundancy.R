12986263d614f8680873a8a0b508a38824b4279a
john <bblodfon@gmail.com>
2025-05-22 20:09:33 +0200

    fix vector names

--- bench/redundancy.R
+++ bench/redundancy.R
@@ -27,15 +27,15 @@ compute_redundancy_per_method = function(row, task_list, omic_id, train_set, met
 
   if (length(selected_features) == 1) {
     redundancy = c(
-      values_pearson = NA,
-      sign_pearson = NA,
-      prop_sign_pearson = NA,
-      values_spearman = NA,
-      sign_spearman = NA,
-      prop_sign_spearman = NA,
-      values_xicor = NA,
-      sign_xicor = NA,
-      prop_sign_xicor = NA
+      rrate_pearson = NA,
+      prop5_pearson = NA,
+      prop1_pearson = NA,
+      rrate_spearman = NA,
+      prop5_spearman = NA,
+      prop1_spearman = NA,
+      rrate_xicor = NA,
+      prop5_xicor = NA,
+      prop1_xicor = NA
     )
   } else {
     data = task_list[[omic_id]]$data(rows = train_set, cols = selected_features)
