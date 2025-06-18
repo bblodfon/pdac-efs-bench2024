3b28bd5f4b2fdbd0f0f42a9d7b4f7480a939aaf4
john <bblodfon@gmail.com>
2025-06-18 15:53:59 +0200

    refactor for simplicity + parallelization

--- bench/redundancy.R
+++ bench/redundancy.R
@@ -4,6 +4,9 @@
 #' We calculate the pair-wise Pearson, Spearman and XI correlation (Chatterjee 2021) coefficients
 #' for every selected feature set and provide the following summary scores:
 #' - Mean absolute correlation between all unique feature pairs (Redundancy Rate or RR).
+#' - The Adjusted Redundancy Rate (chance-corrected, i.e. the adjusted score is
+#' independent of the number of chosen features so FS methods with different number
+#' of selected features can be compared)
 #' - Proportion of FDR-significant pairs (Significant Redundancy Proportion or SRP).
 #' i.e. "What proportion of pairs are significantly redundant?"
 #'
@@ -23,20 +26,18 @@ options(progressr.enable = TRUE)
 handlers(on_missing = "ignore", global = TRUE)
 handlers("progress")
 
+#' Helper function to compute redundancy scores in a given `mlr3` task
 #' `alpha`: FDR threshold for SRP
 #' `N`: number of random feature subsets to consider to correct for chance, i.e.
 #' `N` random feature sets of the same size as the selected features are generated
-compute_redundancy_per_method = function(row, task_list, omic_id, train_set, method,
-                                         alpha = 0.05, N = 1000) {
-  selected_features = row[[method]][[1]]
+compute_redundancy = function(task, train_set, selected_features, alpha = 0.05, N = 1000) {
   p = length(selected_features)
-  task = task_list[[omic_id]]
 
   if (p <= 1) {
-    redundancy = c(
-      rrate_pearson = NA, srp_pearson = NA,
-      rrate_spearman = NA, srp_spearman = NA,
-      rrate_xicor = NA, srp_xicor = NA, adj_xicor = NA
+    redundancy = tibble(
+      rr_pearson = NA, srp_pearson = NA,
+      rr_spearman = NA, srp_spearman = NA,
+      rr_xicor = NA, srp_xicor = NA, arr_xicor = NA
     )
   } else {
     data = task$data(rows = train_set, cols = selected_features)
@@ -55,13 +56,13 @@ compute_redundancy_per_method = function(row, task_list, omic_id, train_set, met
       x = data[[combs[1, i]]]
       y = data[[combs[2, i]]]
 
-      test = cor.test(x, y, method = "pearson")
-      pearson_coeff[i] = test$estimate
-      pearson_pvalues[i] = test$p.value
+      ptest = cor.test(x, y, method = "pearson")
+      pearson_coeff[i] = ptest$estimate
+      pearson_pvalues[i] = ptest$p.value
 
-      test = cor.test(x, y, method = "spearman", exact = FALSE)
-      spearman_coeff[i] = test$estimate
-      spearman_pvalues[i] = test$p.value
+      stest = cor.test(x, y, method = "spearman", exact = FALSE)
+      spearman_coeff[i] = stest$estimate
+      spearman_pvalues[i] = stest$p.value
 
       # choose the max value of xicor, since it's not a symmetric measure of dependence
       # see (Chatterjee 2021) - Remark No. 1
@@ -75,7 +76,7 @@ compute_redundancy_per_method = function(row, task_list, omic_id, train_set, met
     all_data = task$data(rows = train_set)
     xi_null = numeric(N)
     for (i in seq_len(N)) {
-      samp = sample(ncol(all_data), p)
+      samp = sample(ncol(all_data), size = p)
       combs_null = utils::combn(samp, 2)
       xi_vals = numeric(ncol(combs_null))
       for (j in seq_along(xi_vals)) {
@@ -91,84 +92,87 @@ compute_redundancy_per_method = function(row, task_list, omic_id, train_set, met
     xi_obs = mean(abs(xicor_coeff))
     adj_xi = (xi_obs - xi_expected) / (xi_max - xi_expected)
 
-    redundancy = c(
-      #' `rrate` as in "redundancy rate"
+    redundancy = tibble(
+      #' `rr` as in "redundancy rate"
+      #' `arr` as in "adjusted redundancy rate"
+      #' `srp` as in "significant redundancy proportion"
+
       #' Pearson
-      rrate_pearson = mean(abs(pearson_coeff)),
+      rr_pearson = mean(abs(pearson_coeff)),
       srp_pearson = sum(p.adjust(pearson_pvalues, "fdr") < alpha) / n_pairs,
       #' Spearman
-      rrate_spearman = mean(abs(spearman_coeff)),
+      rr_spearman = mean(abs(spearman_coeff)),
       srp_spearman = sum(p.adjust(spearman_pvalues, "fdr") < alpha) / n_pairs,
       #' Xicor
-      rrate_xicor = xi_obs,
+      rr_xicor = xi_obs,
       srp_xicor = sum(p.adjust(xicor_pvalues, "fdr") < alpha) / n_pairs,
-      adj_xicor = adj_xi
+      #' Chance-corrected Xicor
+      arr_xicor = adj_xi
     )
   }
 
   redundancy
 }
 
-compute_redundancy = function(row, task_list, subsampling, methods){
-  redundancy = lapply(methods, function(method) {
-    result = compute_redundancy_per_method(
-      row = row,
-      task_list = task_list,
-      omic_id = row[["omic_id"]],
-      train_set = subsampling$train_set(i = row[["rsmp_id"]]),
-      method = method)
-    names(result) = paste0(gsub("_feats", "", method), "_", names(result))
-
-    result
-  })
-
-  unlist(redundancy)
-}
-
 # load the feature selection results per omic
 fs = readRDS(file = "bench/fs.rds")
-methods = colnames(select(fs, ends_with("_feats")))
+
+fs_long = fs |>
+  pivot_longer(
+    cols = ends_with("_feats"),
+    names_to = "fs_method_id",
+    values_to = "feats"
+  ) |>
+  mutate(fs_method_id = sub("_feats$", "", fs_method_id)) |> # cleanup fs method names
+  select(dataset_id, omic_id, rsmp_id, fs_method_id, feats)
+
+# get task lists beforehand for memory efficiency
 dataset_ids = unique(fs$dataset_id)
-fs_red = list()
+
+task_lists = lapply(dataset_ids, function(id) {
+  readRDS(file.path("data", id, "task_list.rds"))
+})
+names(task_lists) = dataset_ids
+
+# same for subsamplings
+subsamplings = lapply(dataset_ids, function(id) {
+  readRDS(file.path("data", id, "subsampling.rds"))
+})
+names(subsamplings) = dataset_ids
 
 # for reproducibility
 set.seed(42)
 
 # parallelization
-plan(multicore, workers = 15)
-
-fs_red = list()
+plan(multicore, workers = 100)
 
 with_progress({
-  total_rows = nrow(fs)
+  total_rows = nrow(fs_long)
   p = progressor(along = 1:total_rows)
 
-  ## loop over datasets (outer loop sequential, inner parallel)
-  for (dset in dataset_ids) {
+  fs_red = future.apply::future_lapply(seq_len(total_rows), function(i) {
+    data = fs_long[i, ]
+    p(sprintf("Dataset: %s, Omic: %s, Subsampling Iter: %i, FS method: %s",
+              dataset_id, omic_id, rsmp_id, fs_method_id))
 
-    subsampling = readRDS(file.path("data", dset, "subsampling.rds"))
-    task_list   = readRDS(file.path("data", dset, "task_list.rds"))
-    fs_dset     = filter(fs, dataset_id == dset)
+    dataset_id = data$dataset_id
+    omic_id = data$omic_id
+    rsmp_id = data$rsmp_id
+    fs_method_id = data$fs_method_id
+    selected_features = data$feats[[1]]
 
-    red_dset = future.apply::future_lapply(seq_len(nrow(fs_dset)), function(i) {
-      row = fs_dset[i, ]
+    # get task
+    task = task_lists[[dataset_id]][[omic_id]]
 
-      r = compute_redundancy(
-        row         = row,
-        task_list   = task_list,
-        subsampling = subsampling,
-        methods     = methods
-      )
+    # get train set
+    train_set = subsamplings[[dataset_id]]$train_set(i = rsmp_id)
 
-      p(sprintf("%s row %d/%d", dset, i, nrow(fs_dset)))
-      as.data.frame(as.list(r))
-    }, future.seed = TRUE) |> bind_rows()
+    # get redundancy scores
+    r = compute_redundancy(task, train_set, selected_features, N = 10)
 
-    fs_red[[dset]] = bind_cols(fs_dset, red_dset)
-  }
+    bind_cols(data, r) |> select(!feats)
+  }, future.seed = TRUE) |> bind_rows()
 })
-
-fs_red = bind_rows(fs_red) |> select(-ends_with("feats"), -ends_with("coxlasso_train_time"))
 
 # save result to disk
 saveRDS(fs_red, file = "bench/fs_red.rds")
