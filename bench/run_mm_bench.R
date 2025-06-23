02a5bb2f183ebd206fe61c77ab83d00019558f1e
john <bblodfon@gmail.com>
2025-06-23 20:39:05 +0200

    refactor + add new option to NOT do any FS!

--- bench/run_mm_bench.R
+++ bench/run_mm_bench.R
@@ -12,7 +12,8 @@
 #'    to form multi-omics datasets.
 #'    - These datasets are then used to train a model on the training set and
 #'    predict on the test set.
-#'    - Several model options are available (Coxlasso, RSF, CoxPH, BlockForest).
+#'    - Several model options are available (Coxlasso, RSF, CoxPH, BlockForest)
+#'    as well as which data types to integrate.
 #'
 #' Execute: `Rscript bench/run_mm_bench.R` (from project root)
 
@@ -53,14 +54,20 @@ fs = readRDS(file = "bench/fs.rds")
 fs_method_ids = colnames(fs)[endsWith(colnames(fs), "_feats")]
 
 #' Pattern: a-b-c, where:
-#' `a`: integration model for late fusion (or baseline)
-#' `b`: data type(s)
-#' `c`: FS method (if absent or `all` => all FS methods are considered)
-model_data_configs = c(
+#' `a`: Integration model for late fusion (or baseline) => {`coxlasso`, `rsf`, `blockforest`}
+#' `b`: Data type(s) => {`all`, `clinical`, `gex`, `clinical+gex`}.
+#'      Note: `all` means clinical + ALL omics available
+#' `c`: FS method = {`none`, `coxlasso_all_feats`, etc.}.
+#'      Note" If absent, all available FS methods (`fs_method_ids`) are considered.
+model_data_fs_configs = c(
   # Integration Model: CoxLasso, Data: ALL => Clinical + OMICS
   "coxlasso-all",
+  # Integration Model: CoxLasso, Data: ALL => Clinical + OMICS, no FS method
+  "coxlasso-all-none",
   # Integration Model: RSF, Data: ALL => Clinical + OMICS
   "rsf-all",
+  # Integration Model: RSF, Data: ALL => Clinical + OMICS, no FS method
+  "rsf-all-none",
   # (Baseline) Model: CoxPH, Data: Clinical (Reference, for both datasets)
   "cox-clinical",
   # (Baseline) Model: RSF, Data: Clinical (Reference, for both datasets)
@@ -73,6 +80,8 @@ model_data_configs = c(
   #"coxlasso-clinical+gex-coxlasso_feats",
   # Integration Model: BlockForest, Data: ALL
   "blockforest-all",
+  # Integration Model: BlockForest, Data: ALL, no FS method
+  "blockforest-all-none",
   # Integration Model: BlockForest, Data: Clinical + GEX, FS method for GEX: CoxLasso
   "blockforest-clinical+gex-efs_all_feats"
 )
@@ -92,7 +101,7 @@ grid_list = lapply(dataset_ids, function(dataset_id) {
   expand.grid(
     dataset_id = dataset_id,
     fs_method_id = fs_method_ids,
-    model_data_config = model_data_configs,
+    model_data_fs_config = model_data_fs_configs,
     rsmp_id = rsmp_ids,
     stringsAsFactors = FALSE
   )
@@ -103,177 +112,168 @@ grid_df = do.call(rbind, grid_list)
 # filter configurations
 grid_df_filtered = grid_df |>
   # Step 1: osipov2024 dataset doesn't have GEX data
-  filter(!(dataset_id == "osipov2024" & str_detect(model_data_config, "gex"))) |>
+  filter(!(dataset_id == "osipov2024" & str_detect(model_data_fs_config, "gex"))) |>
   # Step 2: for configs with a 3rd part (pattern: a-b-c), ensure 3rd part == fs_method_id
   filter({
-    third_part = str_match(model_data_config, "^[^-]+-[^-]+-(.+)$")[,2]
-    is.na(third_part) | third_part == fs_method_id
+    third_part = str_match(model_data_fs_config, "^[^-]+-[^-]+-(.+)$")[,2]
+    is.na(third_part) | third_part == fs_method_id | third_part == "none"
   })
 
-# Clinical-only config DO NOT do any feature selection
-clinical_only = grid_df_filtered |>
-  filter(model_data_config %in% c("cox-clinical", "rsf-clinical")) |>
-  distinct(dataset_id, model_data_config, rsmp_id) |>
+# Clinical-only and "none" fs config DO NOT do any feature selection
+nofs_df = grid_df_filtered |>
+  filter(str_ends(model_data_fs_config, "clinical") |
+         str_ends(model_data_fs_config, "none")) |>
+  distinct(dataset_id, model_data_fs_config, rsmp_id) |>
   mutate(fs_method_id = NA)
 
 # Extract the rest
 rest = grid_df_filtered |>
-  filter(!model_data_config %in% c("cox-clinical", "rsf-clinical"))
+  filter(!str_ends(model_data_fs_config, "clinical") &
+         !str_ends(model_data_fs_config, "none"))
 
-if (nrow(clinical_only) > 0) {
-  grid_df = bind_rows(clinical_only, rest)
+if (nrow(nofs_df) > 0) {
+  grid_df = bind_rows(nofs_df, rest)
 } else {
   grid_df = rest
 }
 
 # how many configs to run?
-nrow(grid_df)
+cat(sprintf("--- %i configs to run in total ----", nrow(grid_df)))
+
+# helper function to select features
+select_features = function(dataset_id, fs_method_id, rsmp_id, omic_id) {
+  if (fs_method_id == "none" || is.na(fs_method_id)) return(NULL)
+
+  result = fs |>
+    filter(dataset_id == !!dataset_id,
+           omic_id == !!omic_id,
+           rsmp_id == !!rsmp_id) |>
+    pull(fs_method_id)
+
+  if (length(result) == 0) return(NULL)
+
+  result[[1L]]
+}
 
 # Parallelized function for multi-omics benchmark
 mm_bench = function(params, p) {
   set.seed(42)
+
   dataset_id = params$dataset_id
-  fs_method_id = params$fs_method_id
-  model_data_config = params$model_data_config
-  config = strsplit(model_data_config, split = "-")[[1L]]
-  model = config[1]
-  data = config[2]
+  fs_method_id = params$fs_method_id %||% "none" # none or NA means: no FS
+  model_data_fs_config = params$model_data_fs_config
   rsmp_id = params$rsmp_id
 
+  # parse config
+  parts = strsplit(model_data_fs_config, split = "-")[[1L]]
+  model = parts[1]
+  data = parts[2]
+
   #' Notify progress via `p = progressr::progressor()`
-  p(sprintf("Dataset: %s, FS-method: %s, Model: %s, Data: %s, Subsampling Iter: %i",
+  p(sprintf("Dataset: %s, FS-method: %s, Model: %s, Data: %s, SubSmp iter: %i",
             dataset_id, fs_method_id, model, data, rsmp_id))
 
-  # get clinical + ALL omics tasks for this dataset
+  # Load tasks and sub-sampling (train/test split)
   task_list = readRDS(file.path("data", dataset_id, "task_list.rds"))
-
-  # CREATE TASK
-  if (data == "clinical") {
-    # just take the clinical data, no standardization
-    task = task_list$clinical
-  } else if (data == "gex") {
-    # Feature selection for GEX data
-    result = fs |>
-      filter(dataset_id == !!dataset_id,
-             omic_id == "gex",
-             rsmp_id == !!rsmp_id
-      ) |> pull(fs_method_id)
-    gex_features = result[[1L]]
-    all_data = task_list[["gex"]]$data(cols = c("time", "status", gex_features))
-    task = as_task_surv(x = all_data, time = "time", event = "status")
-
-    # standardize data (mean = 0, sd = 1)
-    pos = po("scale")
-    task = pos$train(list(task))[[1L]]
-  } else if (data == "clinical+gex") {
-    # Feature selection for GEX data
-    result = fs |>
-      filter(dataset_id == !!dataset_id,
-             omic_id == "gex",
-             rsmp_id == !!rsmp_id
-      ) |> pull(fs_method_id)
-    gex_features = result[[1L]]
-    # gex_features = task_list[["gex"]]$feature_names # if no FS for GEX data
-
-    gex_data = task_list[["gex"]]$data(cols = gex_features)
-    clinical_data = task_list$clinical$data() # time, status included
-
-    # combine clinical + gex data
-    all_data = cbind(clinical_data, gex_data)
-
-    # make clinical + gex task
-    task = as_task_surv(x = all_data, time = "time", event = "status")
-
-    # standardize data (mean = 0, sd = 1)
-    pos = po("scale")
-    task = pos$train(list(task))[[1L]]
-  } else { # `all`
-    # combine all omics to a combined multi-omics dataset
-    all_data = map_dtc(names(task_list), function(omic_id) {
-      # investigate: remove mutation omic (default FALSE: DON'T DO THIS)
-      if (FALSE && dataset_id == "wissel2023" && omic_id == "mutation") {
-        return(data.table())
-      }
-
-      if (omic_id == "clinical") {
-        # no fs for clinical features
-        return(task_list[[omic_id]]$data())
-      }
-
-      # fs for omic features
-      result = fs |>
-        filter(dataset_id == !!dataset_id,
-               omic_id == !!omic_id,
-               rsmp_id == !!rsmp_id
-        ) |> pull(fs_method_id)
-      selected_features = result[[1L]]
-      task_list[[omic_id]]$data(cols = selected_features)
-    })
-
-    # make multi-omics task
-    task = as_task_surv(x = all_data, time = "time", event = "status")
-
-    # standardize data (mean = 0, sd = 1)
-    pos = po("scale")
-    task = pos$train(list(task))[[1L]]
-  }
-
-  # GET RESAMPLING: train/test split
-  subsampling = readRDS(file = file.path("data", dataset_id, "subsampling.rds"))
+  subsampling = readRDS(file.path("data", dataset_id, "subsampling.rds"))
   train_set = subsampling$train_set(rsmp_id)
   test_set = subsampling$test_set(rsmp_id)
 
-  # CREATE MODEL
-  if (model == "coxlasso") {
-    # CoxLasso
-    #' `standardize = FALSE` as data (train and test set) is already standardized
-    # same config as in Wissel et al. 2023
-    learner = lrn("surv.cv_glmnet", id = model, standardize = FALSE, alpha = 1,
-                  nfolds = 5, type.measure = "deviance", grouped = TRUE, s = "lambda.min")
-  } else if (model == "rsf") {
-    # Random Survival Forest
-    learner = lrn("surv.ranger", id = model, importance = "none",
-                  num.trees = 2000, splitrule = "logrank")
-  } else if (model == "cox") {
-    # Simple Cox PH
-    learner = lrn("surv.coxph")
-  } else if (model == "blockforest") {
-    omic_prefixes = setdiff(names(task_list), "clinical")
-    blocks = get_block_indices(feature_names = task$feature_names,
-                               omic_prefixes = omic_prefixes)
-    learner = lrn("surv.blockforest", blocks = blocks, splitrule = "logrank",
-                  num.trees = 2000, nsets = 300, num.trees.pre = 1000, num.threads = 8)
-  } else {
-    stopf("Model %s not implemented in this benchmark", model)
-  }
+  # === Create Task ===
+  task = switch(data,
+    "clinical" = {
+      task_list$clinical
+    },
+    "gex" = {
+      gex_features = select_features(dataset_id, fs_method_id, rsmp_id, omic_id = "gex")
+      gex_data = task_list$gex$data(
+        cols = c("time", "status", gex_features %||% task_list$gex$feature_names)
+      )
+      task = as_task_surv(x = gex_data, time = "time", event = "status")
 
-  # train model on train set
+      # standardize data (mean = 0, sd = 1)
+      pos = po("scale")
+      pos$train(list(task))[[1L]]
+    },
+    "clinical+gex" = {
+      gex_features = select_features(dataset_id, fs_method_id, rsmp_id, omic_id = "gex")
+      gex_data = task_list$gex$data(
+        cols = gex_features %||% task_list$gex$feature_names
+      )
+      clinical_data = task_list$clinical$data()
+      all_data = cbind(clinical_data, gex_data)
+      task = as_task_surv(x = all_data, time = "time", event = "status")
+
+      # standardize data (mean = 0, sd = 1)
+      pos = po("scale")
+      pos$train(list(task))[[1L]]
+    },
+    "all" = {
+      # combine all omics to a combined multi-omics dataset
+      omic_data = map_dtc(names(task_list), function(omic_id) {
+        # investigate: remove mutation omic (default FALSE: DON'T DO THIS)
+        if (FALSE && dataset_id == "wissel2023" && omic_id == "mutation") {
+          return(data.table())
+        }
+
+        features = select_features(dataset_id, fs_method_id, rsmp_id, omic_id)
+
+        if (omic_id == "clinical") {
+          task_list[[omic_id]]$data()
+        } else {
+          task_list[[omic_id]]$data(cols = features %||% task_list[[omic_id]]$feature_names)
+        }
+      })
+      task = as_task_surv(x = omic_data, time = "time", event = "status")
+
+      # standardize data (mean = 0, sd = 1)
+      pos = po("scale")
+      pos$train(list(task))[[1L]]
+    },
+    stopf("Unsupported data config: %s", data)
+  )
+
+  # === Learner ===
+  learner = switch(model,
+     # CoxLasso
+     #' `standardize = FALSE` as data (train and test set) is already standardized
+     #' same config as in Wissel et al. (2023)
+     "coxlasso" = lrn("surv.cv_glmnet", id = model, standardize = FALSE,
+                      alpha = 1, nfolds = 5, type.measure = "deviance",
+                      grouped = TRUE, s = "lambda.min"),
+     # Random Survival Forest
+     "rsf" = lrn("surv.ranger", id = model, importance = "none",
+                 num.trees = 2000, splitrule = "logrank", num.threads = 3),
+     # Cox Proportional Hazards
+     "cox" = lrn("surv.coxph"),
+     # Block Forest
+     "blockforest" = {
+       omic_prefixes = setdiff(names(task_list), "clinical")
+       blocks = get_block_indices(feature_names = task$feature_names,
+                                  omic_prefixes = omic_prefixes)
+       lrn("surv.blockforest", blocks = blocks, splitrule = "logrank",
+           num.trees = 2000, nsets = 300, num.trees.pre = 1000, num.threads = 8)
+     },
+     stopf("Model %s not implemented", model)
+  )
+
+  # === Fit and Predict ===
   learner$train(task, row_ids = train_set)
+  pred = learner$predict(task, row_ids = test_set)
 
-  # predict with model on test set
-  p = learner$predict(task, row_ids = test_set)
-
-  # measure performance via C-index
-  harrell_c = p$score(msr("surv.cindex"))
-  uno_c = p$score(msr("surv.cindex", weight_meth = "G2"),
-                  task = task, train_set = train_set)
-  # investigate IBS (t_max = 24 months)
-  brier_tmax24 = p$score(msr("surv.graf", t_max = 24, ERV = TRUE),
-                         task = task, train_set = train_set)
-
-  # Return result as a tibble
+  # === Score ===
   tibble(
     dataset_id = dataset_id,
     fs_method_id = fs_method_id,
     rsmp_id = rsmp_id,
-    model_data_config = model_data_config,
-    # include the train task here if needed
-    # task = task$filter(rows = train_set),
+    model_data_config = model_data_fs_config,
     task_nfeats = task$n_features, # how many multi-omics features were used
     task_feats = list(task$feature_names),
-    harrell_c = harrell_c,
-    uno_c = uno_c,
-    brier_tmax24 = brier_tmax24
+    harrell_c = pred$score(msr("surv.cindex")),
+    uno_c = pred$score(msr("surv.cindex", weight_meth = "G2"),
+                       task = task, train_set = train_set),
+    brier_tmax24 = pred$score(msr("surv.graf", t_max = 24, ERV = TRUE),
+                              task = task, train_set = train_set)
   )
 }
 
@@ -295,5 +295,4 @@ execute_bench = function() {
 result = execute_bench()
 
 # Save results
-saveRDS(result, file = "bench/result_bf.rds")
-
+saveRDS(result, file = "bench/result.rds")
